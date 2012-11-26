848e4bd96ce2cedc194e2698c5c9b416db1788bc
Paolo del Mundo <paolodm@gmail.com>
2012-11-25 21:57:01 -0500

    Reimplementation of join.py

--- superjoin-web/join2.py
+++ superjoin-web/join2.py
@@ -3,28 +3,7 @@ import pymongo
 from bson.code import Code
 from itertools import izip
 
-
-
 db = pymongo.Connection()['mr_demo']
-
-life_expect_map  = Code("""
-    function () {
-   emit(this.country, {life_expectancy: this.lifespan });
-    }
-""")
-
-us_econ_map   = Code("""
-    function () {
-  // The data set contains grant amounts going back to 1946.  I
-  // am only interested in 2009 grants.
-  if (this.FY2009 !== undefined && this.FY2009 !== null) {
-    emit(this.country_name, {
-      dollars: this.FY2009
-    });
-  }
-}
-""")
-
 
 def create_map(joined_column, columns):
     
@@ -33,32 +12,27 @@ def create_map(joined_column, columns):
     for column in tuple(columns):
         if not (joined_column == column):
             column_dict[column] = """this.%s""" % column
-    
-    
+
     print ','.join(column_dict)
-    
-    code_text = """
-    function () {
-   emit(this.%s, %s);
-    }
-    """ % (joined_column, str(column_dict).replace("'", ""))
-    
-    map_code = Code(str(code_text))
-    
-    return map_code
+
+    return Code(
+        """
+        function () {
+            emit(this.%s, %s);
+        }
+        """ % (joined_column, str(column_dict).replace("'", ""))
+    )
 
 def create_reducer(joined_column):
- 
-#                 if (!merged.hasOwnProperty(attrname)) 
-    code_text = """
+    return Code(
+    """
     function(key, values) {
     
         var merged = {};
     
         values.forEach(function(value) {
             for (var attrname in value) {
-    
-                    
+                if (!merged.hasOwnProperty(attrname))
                     merged[attrname] = value[attrname];
             }
         });
@@ -67,44 +41,24 @@ def create_reducer(joined_column):
     
         return merged;
     }
-    """ % joined_column
-    
-    reduce_code = Code(code_text)
-    
-    return reduce_code
+    """ % joined_column)
 
 
-
-    
-reducer = Code("""
-function(key, values) {
-
-	var merged = {};
-
-	values.forEach(function(value) {
-    	for (var attrname in value) {
-
-    	    if (!merged.hasOwnProperty(attrname))
-                merged[attrname] = value[attrname];
-    	}
-    });
-
-    merged['country'] = key;
-    merged['creator'] = "paolo";
-
-	return merged;
-}
-""")
-
-if __name__ == '__main__':
-
+def join(join_column, table1, table2):
     slices_collection = db['slices']
-    
-    join_column = "country"
-    life_expect_map = create_map(join_column, ('country','age'))
 
+    transform(join_column, table1, ('country','lifespan'))
+    transform(join_column, table2, ('country','age'))
+
+def transform(join_column, table, columns):
+    life_expect_map = create_map(join_column, columns)
     life_expect_reducer = create_reducer(join_column)
 
-    print life_expect_map
+    print "Map: %s" % life_expect_map
 
-    db.life_expectancy.map_reduce(life_expect_map, life_expect_reducer, {'reduce': 'joined'} )
+    print "Reducer: %s" % life_expect_reducer
+
+    table.map_reduce(life_expect_map, life_expect_reducer, {'reduce': 'joined'} )
+
+if __name__ == '__main__':
+    join('country', db.life_expectancy, db.life_stunted)
